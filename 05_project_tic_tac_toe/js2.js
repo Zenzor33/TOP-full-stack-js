63bfa15335ff01df60e57f27c81a22a3efe0f41a
Zenzor33 <rzeidler@gmail.com>
2022-06-25 09:57:32 -0300

    added method to gameboardcontroller object that updates an array of available squares in-play

--- 05_project_tic_tac_toe/js2.js
+++ 05_project_tic_tac_toe/js2.js
@@ -11,14 +11,54 @@ gameboard module:
 
 // player's factory function
 
+let player1 = {
+  name: "player1",
+  selections: ["1", "3"],
+};
+
+let player2 = {
+  name: "player2",
+  selections: ["4"],
+};
+
 const gameboard = (() => {
   // Initializtion. Retreive the e.target.id
   const playerSelection = (e) => {
     const playerChoice = e.target.id;
     console.log(playerChoice);
   };
+
+  // Create object to store available square ID's
+  let squares = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  let availableSquares = [];
+  let unavailableSquares = [];
+
+  const updateAvailableSquares = () => {
+    for (let i = 0; i < player1.selections.length; i++) {
+      let selection = player1.selections[i];
+      if (
+        squares.includes(selection) &&
+        !unavailableSquares.includes(selection)
+      ) {
+        unavailableSquares.push(selection);
+      }
+    }
+    for (let i = 0; i < player2.selections.length; i++) {
+      let selection = player2.selections[i];
+      if (
+        squares.includes(selection) &&
+        !unavailableSquares.includes(selection)
+      ) {
+        unavailableSquares.push(selection);
+      }
+    }
+  };
+
   return {
     playerSelection,
+    squares,
+    unavailableSquares,
+    updateAvailableSquares,
   };
 })();
 
