ac7b2a3a1c1eb3641a224711b022ff5e274e89aa
Richard Zeidler <rzeidler@gmail.com>
2022-08-12 09:39:29 +0200

    huh2

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,10 +1,29 @@
+// // import your function
+// import myName from './myName';
+
+// function component() {
+//   const element = document.createElement('div');
+
+//   // use your function!
+//   element.textContent = myName('Cody');
+//   return element;
+// }
+
+// document.body.appendChild(component());
+
+import _ from 'lodash';
+
+
 function component() {
     const element = document.createElement('div');
   
-    // Lodash, currently included via a script, is required for this line to work
+   // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
+
     return element;
   }
   
   document.body.appendChild(component());
+
+// import your function
+import myName from './myName';
