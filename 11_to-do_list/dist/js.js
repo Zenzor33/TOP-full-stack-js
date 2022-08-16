08f6de043ba5d2fdda14f7ea852769c13703547d
Richard Zeidler <rzeidler@gmail.com>
2022-08-16 13:44:43 +0200

    layout- finished pop-up form

--- 11_to-do_list/dist/js.js
+++ 11_to-do_list/dist/js.js
@@ -1,1 +1,15 @@
-console.log("hello");
+/*
+
+Algorithm:
+- Add event listener to add project button
+- On click, it displays a popup overlay with a text field with label: 'Project Name' and two buttons: 'Submit' and 'Cancel'.
+
+*/
+
+const btnAddProject = document.getElementById("btn-add-project");
+
+btnAddProject.addEventListener("click", displayPopupProject);
+
+function displayPopupProject() {
+  console.log("helo");
+}
